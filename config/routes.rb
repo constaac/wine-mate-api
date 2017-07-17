e9a1ac7cf9f7cd3dc2da04984fa01daa7df76567
constaac <aconstantine719@gmail.com>
2017-07-17 12:44:28 -0400

    Configures Routes
    Removes index/show on Users route
    
    Appends ', except: [:new, :edit]' to WL/Inv resources
    
    Subject to change to accommodate front-end
    
    closes #17

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  resources :inventories
-  resources :wish_lists
+  resources :inventories, except: [:new, :edit]
+  resources :wish_lists, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
-  resources :users, only: [:index, :show]
 end
